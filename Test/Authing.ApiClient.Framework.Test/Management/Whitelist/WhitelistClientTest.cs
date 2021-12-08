6dc7b8cc4bcd49bc890bb5022a46a3ab852626ec
qidong5566 <635877990@qq.com>
2021-12-08 11:06:24 +0800

    1.添加文件

---
+++ Test/Authing.ApiClient.Framework.Test/Management/Whitelist/WhitelistClientTest.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Authing.ApiClient.Types;
+using Xunit;
+
+namespace Authing.ApiClient.Framework.Test.ManagementClient.Whitelist
+{
+    public class WhitelistClienTtest : BaseTest
+    {
+        [Fact]
+        public async void enable_whitelist_Func()
+        {
+            var client = managementClient;
+            var res = await client.Whitelist.Enable(WhitelistType.EMAIL | WhitelistType.PHONE | WhitelistType.USERNAME);
+            Assert.True(res.Result.Whitelist.EmailEnabled & res.Result.Whitelist.PhoneEnabled & res.Result.Whitelist.UsernameEnabled);
+        }
+
+        [Fact]
+        public async void disable_whitelist_Func()
+        {
+            var client = managementClient;
+            var res = await client.Whitelist.Disable(WhitelistType.EMAIL | WhitelistType.PHONE | WhitelistType.USERNAME);
+            Assert.False(res.Result.Whitelist.EmailEnabled & res.Result.Whitelist.PhoneEnabled & res.Result.Whitelist.UsernameEnabled);
+        }
+
+        [Fact]
+        public async void add_whitelist_Func()
+        {
+            List<string> phones = new List<string>();
+            phones.Add("188888888888");
+
+            var client = managementClient;
+            var result = await client.Whitelist.Add(WhitelistType.PHONE, phones);
+            foreach (var phone in phones)
+            {
+                Assert.NotNull(result.FirstOrDefault(c => c.Value == phone));
+            }
+            await client.Whitelist.Remove(WhitelistType.PHONE, phones);
+        }
+
+        [Fact]
+        public async void remove_whitelist_Func()
+        {
+            List<string> phones = new List<string>();
+            phones.Add("188888888888");
+
+            var client = managementClient;
+            await client.Whitelist.Add(WhitelistType.PHONE, phones);
+            var result = await client.Whitelist.Remove(WhitelistType.PHONE, phones);
+            foreach (var phone in phones)
+            {
+                Assert.Null(result.FirstOrDefault(c => c.Value == phone));
+            }
+        }
+
+        [Fact]
+        public async void get_whitelist_Func()
+        {
+            List<string> phones = new List<string>();
+            phones.Add("188888888888");
+
+            var client = managementClient;
+            await client.Whitelist.Add(WhitelistType.PHONE, phones);
+            var result = await client.Whitelist.List( WhitelistType.PHONE);
+            Assert.NotEmpty(result);
+            await client.Whitelist.Remove(WhitelistType.PHONE, phones);
+        }
+    }
+}
