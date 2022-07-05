8340abb5332dd3b425eaecd6bb03a35928fe25f5
TMGG <tm574378328@gmail.com>
2022-07-05 14:05:32 +0800

    底层增加 API V3 方法，增加设置用户自定义字段方法

---
+++ Authing.ApiClient/Domain/Model/V3Model/SetCustomDataParam.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Authing.Library.Domain.Model.V3Model
+{
+    public class SetCustomDataParam
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public TargetType targetType { get; set; }
+        public string targetIdentifier { get; set; }
+        [JsonProperty("namespace")]
+        public string _namespace { get; set; }
+        public List<Dic> list { get; set; }
+    }
+
+    public class Dic
+    {
+        public string key { get; set; }
+        public object value { get; set; }
+    }
+
+    public enum TargetType
+    {
+        USER,
+        ROLE,
+        GROUP,
+        DEPARTMENT
+    }
+
+}
---
+++ Test/Authing.ApiClient.Framework.Test/Management/Orgs/SetPartMentCustomDataTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authing.Library.Domain.Model.V3Model;
+using Xunit;
+
+namespace Authing.ApiClient.Framework.Test.Management.Orgs
+{
+    public class SetPartMentCustomDataTest : BaseTest
+    {
+
+        [Fact]
+        public async void SetPartMentCustomData_Test()
+        {
+            var client = managementClient;
+
+            var res = await client.Orgs.SearchNodes("O1-C");
+
+            var result = await client.Orgs.SetPartMentCustomData<SetCustomDataResponse>(res.First().Id, "isSecurity", "123");
+            //result = await client.Orgs.SetPartMentCustomData<SetCustomDataResponse>(res.First().Id, "isDistribution", "123");
+            Assert.NotNull(result.statusCode == 200);
+        }
+
+    }
+}
