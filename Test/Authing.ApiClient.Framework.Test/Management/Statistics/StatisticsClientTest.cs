5d4db431f7d3c159600ef6094d15a97dbae4f3de
TMGG <tangming@authing.cn>
2022-08-09 10:20:18 +0800

    test:管理模块:更新管理日志统计信息测试结果

--- Test/Authing.ApiClient.Framework.Test/Management/Statistics/StatisticsClientTest.cs
+++ Test/Authing.ApiClient.Framework.Test/Management/Statistics/StatisticsClientTest.cs
@@ -7,17 +7,23 @@ namespace Authing.ApiClient.Framework.Test.Management.Statistics
 {
     public class StatisticsClientTest : BaseTest
     {
+        /// <summary>
+        /// 2022-8-9 测试通过
+        /// </summary>
         [Fact]
         public async Task Statistics_UserLogs()
         {
             var result = await managementClient.Statistics.
                 listUserActions(new LogsPageParam()
                 {
-                    UserId = new List<string>() { "61728068c7e3e10ca9cbe8a9" }
+                    UserId = new List<string>() { "61a5c55fc89ff91083293e45" }
                 });
             Assert.NotEmpty(result.List);
         }
 
+        /// <summary>
+        /// 2022-8-9 测试通过
+        /// </summary>
         [Fact]
         public async Task Statistics_listAuditLogs()
         {
