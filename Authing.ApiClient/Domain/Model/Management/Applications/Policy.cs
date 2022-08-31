dbc8191403b8f4673dd40eebeb38f04fb96db064
TangMing <tangming@authing.cn>
2022-08-31 16:58:28 +0800

    fix: Update policy class

--- Authing.ApiClient/Domain/Model/Management/Applications/Policy.cs
+++ Authing.ApiClient/Domain/Model/Management/Applications/Policy.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using Authing.ApiClient.Types;
 using Newtonsoft.Json;
 
 namespace Authing.ApiClient.Domain.Model.Management.Applications
 {
     #region Policy
-    public class Policy
+    public class PolicyOld
     {
         #region members
         /// <summary>
@@ -58,4 +59,64 @@ namespace Authing.ApiClient.Domain.Model.Management.Applications
     #region PolicyAssignment
 
     #endregion
+
+    public class Policy
+    {
+        public DateTime assignedAt { get; set; }
+        public object inheritByChildren { get; set; }
+        public bool enabled { get; set; }
+        public string policyId { get; set; }
+        public string code { get; set; }
+        public PolicyInfo policy { get; set; }
+        public string targetNamespace { get; set; }
+        public PolicyAssignmentTargetType targetType { get; set; }
+        public string targetIdentifier { get; set; }
+        public Target target { get; set; }
+        public string _namespace { get; set; }
+        public string namespaceName { get; set; }
+        public string namesapceDes { get; set; }
+    }
+
+    public class PolicyInfo
+    {
+        public string id { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+        public string userPoolId { get; set; }
+        public bool isDefault { get; set; }
+        public bool isAuto { get; set; }
+        public bool hidden { get; set; }
+        public string code { get; set; }
+        public string description { get; set; }
+        public Statement[] statements { get; set; }
+        public int namespaceId { get; set; }
+    }
+
+    public class Statement
+    {
+        public object condition { get; set; }
+        public string id { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+        public string userPoolId { get; set; }
+        public string policyId { get; set; }
+        public string resource { get; set; }
+        public object resourceType { get; set; }
+        public string[] actions { get; set; }
+        public string effect { get; set; }
+    }
+
+    public class Target
+    {
+        public string id { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+        public string userPoolId { get; set; }
+        public string code { get; set; }
+        public object description { get; set; }
+        public object parentCode { get; set; }
+        public bool isSystem { get; set; }
+        public int namespaceId { get; set; }
+    }
+
 }
